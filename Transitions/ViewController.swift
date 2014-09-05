6592f31258a30d9a393d0e66e529213dc38b333d
Mathew Sanders <mathew.sanders@gmail.com>
2014-09-05 17:15:36 -0400

    Added exit segue
    An exit segue allows us to unwind one or more previously made segues to
    return to an earlier view controller

--- Transitions/ViewController.swift
+++ Transitions/ViewController.swift
@@ -19,7 +19,10 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func unwindToStart(segue: UIStoryboardSegue) {
+        // empty for now
+    }
 
 }
 
