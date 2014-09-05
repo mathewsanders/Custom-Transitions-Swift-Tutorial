53d6325515ebe2846ef84f429874461feda02bfd
Mathew Sanders <mathew.sanders@gmail.com>
2014-09-05 17:25:02 -0400

    Added required methods for protocols
    Added methods required for TransitionManager to adhere to
    UIViewControllerTransitioningDelegate and
    UIViewControllerAnimatedTransitioning protocols

--- Transitions/TransitionManager.swift
+++ Transitions/TransitionManager.swift
@@ -8,6 +8,30 @@
 
 import UIKit
 
-class TransitionManager: NSObject {
+class TransitionManager: NSObject, UIViewControllerTransitioningDelegate, UIViewControllerAnimatedTransitioning {
+    
+    // MARK: UIViewControllerTransitioningDelegate methods
+    
+    // what UIViewControllerAnimatedTransitioning object to use for presenting
+    func animationControllerForPresentedController(presented: UIViewController!, presentingController presenting: UIViewController!, sourceController source: UIViewController!) -> UIViewControllerAnimatedTransitioning! {
+        return self
+    }
+    
+    // what UIViewControllerAnimatedTransitioning object to use for presenting
+    func animationControllerForDismissedController(dismissed: UIViewController!) -> UIViewControllerAnimatedTransitioning!  {
+        return self
+    }
+    
+    // MARK: UIViewControllerAnimatedTransitioning methods
+    
+    // return how many seconds the transition animation should take
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning!) -> NSTimeInterval  {
+        return 0.5
+    }
+    
+    // perform the animation(s) to show the transition from one screen to another
+    func animateTransition(transitionContext: UIViewControllerContextTransitioning!) {
+        // TODO: we still need to define the animation
+    }
    
 }
