0775314cc61ada94e93808220af158824f1a9d7d
Mathew Sanders <mathew.sanders@gmail.com>
2014-09-05 17:23:19 -0400

    Created TransitionManager class
    The TransitionManager will manage our custom animation between two
    view controllers

---
+++ Transitions/TransitionManager.swift
@@ -0,0 +1,13 @@
+//
+//  TransitionManager.swift
+//  Transitions
+//
+//  Created by Mathew Sanders on 9/5/14.
+//  Copyright (c) 2014 Mat. All rights reserved.
+//
+
+import UIKit
+
+class TransitionManager: NSObject {
+   
+}
